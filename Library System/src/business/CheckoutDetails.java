72d51f231c3593c884f4bd6cedebb1689669e41d
namlih <hasanburaknamli@gmail.com>
2022-03-01 15:03:36 -0600

    Committing rest of the business package classes

---
+++ Library System/src/business/CheckoutDetails.java
@@ -0,0 +1,79 @@
+package business;
+
+import java.util.Date;
+
+public class CheckoutDetails {
+
+    private String isbn;
+    private String title;
+    private Date checkoutDate;
+    private int copyNum;
+    private String member;
+    private Date dueDate;
+
+    public CheckoutDetails(String isbn, String title, int copyNum, Date checkoutDate, Date dueDate) {
+        this.isbn = isbn;
+        this.title = title;
+        this.copyNum = copyNum;
+        this.checkoutDate = checkoutDate;
+        this.dueDate = dueDate;
+    }
+
+    public CheckoutDetails(String isbn, String title, int copyNum, String member, Date dueDate) {
+        this.isbn = isbn;
+        this.title = title;
+        this.copyNum = copyNum;
+        this.member = member;
+        this.dueDate = dueDate;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getCheckoutDate() {
+        return checkoutDate;
+    }
+
+    public void setCheckoutDate(Date checkoutDate) {
+        this.checkoutDate = checkoutDate;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public int getCopyNum() {
+        return copyNum;
+    }
+
+    public void setCopyNum(int copyNum) {
+        this.copyNum = copyNum;
+    }
+
+    public String getMember() {
+        return member;
+    }
+
+    public void setMember(String member) {
+        this.member = member;
+    }
+
+
+}
---
+++ Library System/src/business/LibraryMember.java
@@ -0,0 +1,36 @@
+package business;
+
+import java.io.Serializable;
+
+final public class LibraryMember extends Person implements Serializable {
+    private String memberId;
+    private CheckoutRecord checkoutRecord;
+
+    private static final long serialVersionUID = -2226197306790714013L;
+
+    public LibraryMember(String memberId, String fname, String lname, String tel,Address add) {
+        super(fname,lname, tel, add);
+        this.memberId = memberId;
+        this.setCheckoutRecord(new CheckoutRecord());
+    }
+
+
+    public String getMemberId() {
+        return memberId;
+    }
+
+    @Override
+    public String toString() {
+        return "Member Info: " + "ID: " + memberId + ", name: " + getFirstName() + " " + getLastName() +
+                ", " + getTelephone() + " " + getAddress();
+    }
+
+    public CheckoutRecord getCheckoutRecord() {
+        return checkoutRecord;
+    }
+
+    public void setCheckoutRecord(CheckoutRecord checkoutRecord) {
+        this.checkoutRecord = checkoutRecord;
+    }
+
+}
